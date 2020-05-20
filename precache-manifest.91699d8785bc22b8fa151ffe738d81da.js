fda59635f82817b7758276cafde11b45cad039a9
Andrew Sova <asovacloud@gmail.com>
2020-05-20 15:12:35 +0300

    Updates

---
+++ precache-manifest.91699d8785bc22b8fa151ffe738d81da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35228ef9d35c9443908b14c14e7df379",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "a92006e854e989c0561d",
+    "url": "/todo-list/static/css/main.5083d479.chunk.css"
+  },
+  {
+    "revision": "3fe9f8a1d4fade360c43",
+    "url": "/todo-list/static/js/2.731454ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.731454ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92006e854e989c0561d",
+    "url": "/todo-list/static/js/main.7430df57.chunk.js"
+  },
+  {
+    "revision": "da2a803dcedd98b44d11",
+    "url": "/todo-list/static/js/runtime-main.a19f0868.js"
+  },
+  {
+    "revision": "b3348c9a9fc203dffe6c67c0cce08015",
+    "url": "/todo-list/static/media/logo.b3348c9a.svg"
+  }
+]);
